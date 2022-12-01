ea89b262c9fc0f6903268f43d8d323815b87afb3
avexyy <113837032+avexyy@users.noreply.github.com>
2022-12-01 11:12:58 +0100

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,29 @@
+﻿System.Console.Write("Enter a integer number ( > 0): ");
+int n = int.Parse(Console.ReadLine()!);
+
+System.Console.Write("Enter a second integer number ( > 0): ");
+int a = int.Parse(Console.ReadLine()!);
+
+System.Console.Write("Enter a third integer number ( > 0): ");
+int b = int.Parse(Console.ReadLine()!);
+
+
+for (int i = 1; i <= n; i++)
+{
+    if (i % a == 0 && i % b == 0)
+    {
+        System.Console.Write("FizzBuzz,  ");
+    }
+    else if (i % a == 0)
+    {
+        System.Console.Write("Fizz,  ");
+    }
+    else if (i % b == 0)
+    {
+        System.Console.Write("Buzz,  ");
+    }
+    else
+    {
+        System.Console.Write($"{i},  ");
+    }
+}
