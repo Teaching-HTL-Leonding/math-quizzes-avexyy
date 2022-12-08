de8c6a5a1a8d29b85b01ec0c6dfad877a89cdc4c
avexyy <113837032+avexyy@users.noreply.github.com>
2022-12-08 12:45:15 +0100

    Add files via upload

---
+++ CheckIfPalindrome.cs
@@ -0,0 +1,26 @@
+﻿System.Console.Write("Enter a positive integer number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int tmp = number;
+int reverse = 0;
+int count;
+
+while (number != 0)
+{
+    count = number % 10;
+    reverse = reverse * 10 + count;
+    number /= 10;
+}
+
+if (tmp < 0)
+{
+    System.Console.WriteLine("Invalid Input!");
+}
+else if (reverse == tmp)
+{
+    System.Console.WriteLine($"The number {tmp} is a palindrome.");
+}
+else
+{
+    System.Console.WriteLine($"The number {tmp} is not a palindrome.");
+}
